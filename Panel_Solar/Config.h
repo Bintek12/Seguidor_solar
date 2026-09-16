a9ec084a3a02b1b5f8e230dff12b56b748c1908e
Bruno Gonzalez Rojas <bruno.cm8bak.gmail.com>
2026-09-15 22:49:29 -0400

    Salva para revisar otras ramas

--- Panel_Solar/Config.h
+++ Panel_Solar/Config.h
@@ -10,12 +10,14 @@
 #define MOTOR_DIR   PB7   // Pin de dirección (1=Este, 0=Oeste)
 
 // ---------- Canales ADC para LDR ----------
-#define LDR_ESTE   4      // ADC0
-#define LDR_OESTE  5      // ADC1
+#define LDR_ESTE   4      // ADC4
+#define LDR_OESTE  5      // ADC5
 
 // ---------- Pines de alarma y límite ----------
 #define ALARMA_PIN  0     // PB0
-#define LIMITE_PIN  1     // PB1
+#define LIMITE_PIN_E  1     // PC1    Limite al este
+#define LIMITE_PIN_H  1     // PC2    Limite Horizontal
+#define LIMITE_PIN_W  1     // PC3    Limite al Oeste
 
 // ---------- RS485 enable ----------
 #define RS485_EN   PD3  // El mismo que el led
